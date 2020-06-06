a58a6811ea046180ab2238874d205ec44e99c8e9
Rizal Ibnu <muhamadrizalibnuabdulah@gmail.com>
2020-06-06 20:53:02 +0700

    refactor(danger-plugin-npm-check-updates): release config

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.export = {
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "docs", scope: "README", release: "patch" },
+          { type: "refactor", scope: "core-*", release: "minor" },
+          { type: "refactor", release: "patch" },
+          { type: "style", release: "patch" },
+          { scope: "no-release", release: false },
+        ],
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+  ],
+};
